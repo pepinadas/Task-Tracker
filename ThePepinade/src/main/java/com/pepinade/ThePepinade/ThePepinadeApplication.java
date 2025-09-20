060bbc795bf4dfe1ebcef848a2148f7b90ca48c8
pepinadas <mmmunapregunta@gmail.com>
2025-09-19 21:27:20 -0600

    feat: primer hello world
    hu: pepinadas 19/09/2025

---
+++ ThePepinade/src/main/java/com/pepinade/ThePepinade/ThePepinadeApplication.java
@@ -0,0 +1,13 @@
+package com.pepinade.ThePepinade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePepinadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePepinadeApplication.class, args);
+	}
+
+}
---
+++ ThePepinade/src/test/java/com/pepinade/ThePepinade/ThePepinadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepinade.ThePepinade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePepinadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
